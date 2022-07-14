a6e5c391a8fae980375f9a93f6b6de697d170105
andre-sequeira10 <44123691+andre-sequeira10@users.noreply.github.com>
2022-07-14 22:40:12 +0100

    new example

--- quantum_tree_search_test.py
+++ quantum_tree_search_test.py
@@ -17,23 +17,7 @@ transition_kernel[1] = [(1,3) , (2,4)]
 
 for i in range(2,states):
 	transition_kernel[i] = []
-'''
 
-states=5
-actions=4
-action_set=list(range(actions))
-
-transition_kernel= np.zeros(states,dtype=object)
-steps = 1
-
-#action, sprime
-transition_kernel[0] = [(0,1),(1,2),(2,3)]
-transition_kernel[1] = [(1,4) , (3,5)]
-transition_kernel[2] = []
-transition_kernel[3] = [(1,6),(2,7),(3,8)]
-
-for i in range(4,states):
-	transition_kernel[i] = []
 
 	
 qts = QTS(tree=transition_kernel, n_states=states, action_set=action_set)
@@ -41,7 +25,32 @@ qts = QTS(tree=transition_kernel, n_states=states, action_set=action_set)
 q_tree = qts.traverse(depth=2, mode="depth")
 counts, actions = qts.measure(goal_state=4, iterations=1)
 
+'''
 
+states=7
+actions=3
+action_set=list(range(actions))
+
+transition_kernel= np.zeros(states,dtype=object)
+steps = 1
+
+#action, sprime
+transition_kernel[0] = [(0,1),(1,2)]
+transition_kernel[1] = [(0,6), (1,3) , (2,4)]
+transition_kernel[2] = [(0,4),(1,5)]
+
+for i in range(3,states):
+	transition_kernel[i] = []
+	
+qts = QTS(tree=transition_kernel, n_states=states, action_set=action_set)
+
+#circuit = qts.A([0], constant_branching=False)
+
+q_tree = qts.traverse(depth=2, mode="depth")
+
+#q_tree = qts.traverse(mode="iterative_deepning")
+
+counts, actions = qts.measure(goal_state=4, iterations=None, shots=5000)
 print(actions)
 plot_histogram(counts)
 
